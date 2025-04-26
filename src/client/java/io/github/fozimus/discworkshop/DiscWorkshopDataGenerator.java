cab920e2c3afe31657f4ee9d60036ffc6ee99053
fozimus <francytecno2004@gmail.com>
2025-04-26 17:40:08 +0200

    Inital commit

---
+++ src/client/java/io/github/fozimus/discworkshop/DiscWorkshopDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.fozimus.discworkshop;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DiscWorkshopDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
