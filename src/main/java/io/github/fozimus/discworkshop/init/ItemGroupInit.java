4e2c221ef2a51be6e39a07efd32c30933ffb7c14
fozimus <francytecno2004@gmail.com>
2025-04-30 22:21:40 +0200

    Changed the item group icon to the disc workshop block

--- src/main/java/io/github/fozimus/discworkshop/init/ItemGroupInit.java
+++ src/main/java/io/github/fozimus/discworkshop/init/ItemGroupInit.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import io.github.fozimus.discworkshop.DiscWorkshop;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -13,21 +14,24 @@ public class ItemGroupInit {
     public static final Text DISC_WORKSHOP_GROUP_TITLE = Text.translatable("itemgroup." + DiscWorkshop.MOD_ID + ".disc_workshop");
     public static final ItemGroup DISC_WORKSHOP_GROUP = register("disc_workshop", FabricItemGroup.builder()
                                                                  .displayName(DISC_WORKSHOP_GROUP_TITLE)
-                                                                 .icon(ItemInit.MUSIC_DISC::getDefaultStack)
-                                                                 .entries((displayContext, entries) ->
-                                                                          Registries.ITEM.getIds()
-                                                                          .stream()
-                                                                          .filter(key -> key.getNamespace().equals(DiscWorkshop.MOD_ID))
-                                                                          .map(Registries.ITEM::getOrEmpty)
-                                                                          .map(Optional::orElseThrow)
-                                                                          .forEach(entries::add))
+                                                                 .icon(() -> new ItemStack(BlockInit.DISC_WORKSHOP_BE_BLOCK))
+                                                                 .entries(ItemGroupInit::collectEntries)
                                                                  .build());
 
     public static <T extends ItemGroup> T register(String name, T itemGroup) {
         return Registry.register(Registries.ITEM_GROUP, DiscWorkshop.id(name), itemGroup);
     }
 
+    private static void collectEntries(ItemGroup.DisplayContext displayContext, ItemGroup.Entries entries) {
+        Registries.ITEM.getIds()
+            .stream()
+            .filter(key -> key.getNamespace().equals(DiscWorkshop.MOD_ID))
+            .map(Registries.ITEM::getOrEmpty)
+            .map(Optional::orElseThrow)
+            .forEach(entries::add);
+    }
+        
     public static void init() {
-        DiscWorkshop.LOGGER.info("Registering item groups for {}", DiscWorkshop.MOD_ID);
+        DiscWorkshop.LOGGER.info("Registering item groups for {}", DiscWorkshop.MOD_ID); 
     }
 }
